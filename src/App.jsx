f97df799e3ba461074cdb0698c9c3df5059dd5e1
judaa12 <juan.garciac@ucuenca.edu.ec>
2023-04-23 14:16:12 -0500

    eslint by default changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,57 @@
 import './App.css'
 import { TwitterFollowCard } from './TwitterFollowCard'
 
-
 const users = [
-    {
-        id: 1,
-        userName: "midudev",
-        name: "Miguel Angel Duran",
-        isFollowing: true
-    },
-    {
-        id: 2,
-        userName: "jose",
-        name: "Jose",
-        isFollowing: false
-    },
-    {
-        id: 3,
-        userName: "judatop",
-        name: "Juan Garcia",
-        isFollowing: true
-    },
-    {
-        id:4,
-        userName: "cquito4",
-        name: "Cristian Quito",
-        isFollowing: false
-    }
+  {
+    id: 1,
+    userName: 'midudev',
+    name: 'Miguel Angel Duran',
+    isFollowing: true
+  },
+  {
+    id: 2,
+    userName: 'jose',
+    name: 'Jose',
+    isFollowing: false
+  },
+  {
+    id: 3,
+    userName: 'judatop',
+    name: 'Juan Garcia',
+    isFollowing: true
+  },
+  {
+    id: 4,
+    userName: 'cquito4',
+    name: 'Cristian Quito',
+    isFollowing: false
+  },
+  {
+    id: 5,
+    userName: 'PopBase',
+    name: 'Cristian Quito',
+    isFollowing: false
+  }
 ]
 
-
-export function App() {
-
-    return (
-        <section className="App">
-            {
+export function App () {
+  return (
+    <section className='App'>
+      {
                 users.map(user => {
-                    const {id, userName, name, isFollowing} = user
-                    return (
-                        <TwitterFollowCard 
-                        userName={userName} 
-                        initialIsFollowing={isFollowing}
-                        key={id}
-                        >
-                            {name}
-                        </TwitterFollowCard>
-                    )
+                  const { id, userName, name, isFollowing } = user
+                  return (
+                    <TwitterFollowCard
+                      userName={userName}
+                      initialIsFollowing={isFollowing}
+                      key={id}
+                    >
+                      {name}
+                    </TwitterFollowCard>
+                  )
                 })
             }
 
-        </section>
-    )
+    </section>
+  )
 }
